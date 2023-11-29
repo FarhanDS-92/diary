2c1b6929925abdebb97dc3f837eb301c609a4288
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-29 13:54:53 -0500

    minor change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 import validator from "is-my-date-valid";
+import { useState } from "react";
 
 export default function Home() {
+  const [currentDay, setCurrentDay] = useState("");
+
   return <main></main>;
 }
