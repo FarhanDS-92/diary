9f8c40108bc325d0cb24296c5715263c1515a4a1
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-29 14:04:23 -0500

    checking

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,14 @@ import validator from "is-my-date-valid";
 import { useState } from "react";
 
 export default function Home() {
-  const [currentDay, setCurrentDay] = useState("");
+  const [presentDay, setPresentDay] = useState("");
+
+  const currentDate = new Date();
+  let currentDay = currentDate.getDate();
+  let currentMonth = currentDate.getMonth() + 1;
+  let currentYear = currentDate.getFullYear();
+
+  console.log(currentDate, currentDay, currentMonth, currentYear);
 
   return <main></main>;
 }
